f55ecff44d67afcb3b3784cb8e1c3e0b94dd37d1
Brandon Richey <diamondgfx@gmail.com>
2014-08-11 01:01:29 -0400

    More work with transition scenes and whatnot.

--- Assets/Scripts/TitleScreenScript.cs
+++ Assets/Scripts/TitleScreenScript.cs
@@ -9,15 +9,18 @@ public class TitleScreenScript : MonoBehaviour {
 	void OnGUI()
 	{
 		if (GUI.Button (new Rect (115, 350, 250, 49), newGame, GUIStyle.none)) {
+			PlayerPrefs.SetInt ("TotalScore", 0);
+			PlayerPrefs.SetInt ("Level", 0);
+			PlayerPrefs.Save ();
 			Application.LoadLevel (1);
 		}
 
 		if (GUI.Button (new Rect (115, 415, 250, 49), continueGame, GUIStyle.none)) {
-			print ("you clicked the icon");
+			Application.LoadLevel (1);
 		}
 
 		if (GUI.Button (new Rect (115, 480, 250, 49), endless, GUIStyle.none)) {
-			print ("you clicked the icon");
+			print ("Endless Mode is not implemented yet");
 		}
 	}
 }
---
+++ Assets/Scripts/TransitionScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionScript : MonoBehaviour {
+	public Texture nextLevel;
+	public Texture exit;
+	
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (100, 300, 310, 49), nextLevel, GUIStyle.none)) {
+			Application.LoadLevel (1);
+		}
+		
+		if (GUI.Button (new Rect (190, 415, 122, 49), exit, GUIStyle.none)) {
+			Application.LoadLevel (0);
+		}
+	}
+}
